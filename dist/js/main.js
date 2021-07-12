4a4fddcd5588b1840d86dfef5d02e56e7379e066
fariz <farizrachmansyah7@gmail.com>
2021-07-12 22:38:10 +0700

    bismillah lolos aamiinn

--- dist/js/main.js
+++ dist/js/main.js
@@ -132,4 +132,44 @@ document.addEventListener('DOMContentLoaded', () => {
   ui.burgerMenu();
   ui.imageCarousel();
   ui.formValidation();
+
 });
+
+// SLIDER LOGIC
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// ngatur next dan previous
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// ngatur gambar
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].className += " active";
+}
